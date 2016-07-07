fec5fcfbfcabbfe949f2ab95c2519837dc92a424
Jelly13 <jelisaveta.jelly.markovic@gmail.com>
2016-07-07 15:50:52 +0200

    Adding visibility for textview.

--- app/src/main/java/com/jelisaveta/randomgenerator/MainActivity.java
+++ app/src/main/java/com/jelisaveta/randomgenerator/MainActivity.java
@@ -21,24 +21,34 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText max_broj = (EditText)findViewById(R.id.unetibroj);
         final Button dugme = (Button)findViewById(R.id.dugmence);
+        final TextView izlaz = (TextView)findViewById(R.id.ekran);
+        izlaz.setVisibility(View.INVISIBLE);
         dugme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int max = 6;
+
+                int max = 6; //definisanje maksimalne vrednosti zbog hvatanja exceptiona
+
+                //hvatanje exceptiona
                 try {
                     max = parseInt(max_broj.getText().toString());
                 }
                 catch (NumberFormatException e){
                     Toast.makeText(MainActivity.this, "Invalid number", Toast.LENGTH_SHORT).show();
+                    return;
                 }
                 int MIN = 1;
+                //generisanje random broja
                 Random rand = new Random();
                 int r_broj = rand.nextInt(max-MIN+1) + MIN;
-                TextView izlaz = (TextView)findViewById(R.id.ekran);
+                //Definisanje prikazivanja random broja na ekranu
+
                 izlaz.setText(getResources().getString(R.string.random_broj_je, r_broj));
+                izlaz.setVisibility(View.VISIBLE);
+                //izlaz.setVisibility(View.VISIBLE);
                 //Toast t = Toast.makeText(MainActivity.this, r_broj+"", Toast.LENGTH_SHORT);
                 //t.show();
-                
+
             }
         });
     }
